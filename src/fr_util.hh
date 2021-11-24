d180fc9f404d798ac9e055de1233d087fb7f545f
Ruobing Han <hanruobing@gatech.edu>
2021-11-24 11:23:51 -0500

    use flush+flush, low accuracy

--- src/fr_util.hh
+++ src/fr_util.hh
@@ -1,26 +1,26 @@
-/* Initial Handshake of Sender & Receiver Adapted from Flush=Reload Code at "https://github.com/yshalabi/covert-channel-tutorial". */
+/* Initial Handshake of Sender & Receiver Adapted from Flush=Reload Code at
+ * "https://github.com/yshalabi/covert-channel-tutorial". */
 /*
  */
 
 #ifndef FR_UTIL_H_
 #define FR_UTIL_H_
 
-#include <stdio.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <stdarg.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <getopt.h>
+#include <inttypes.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <inttypes.h>
-#include <time.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/mman.h>
-#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
-#include <errno.h>
-#include <string.h>
 
 #include "utils.hh"
 
@@ -29,15 +29,14 @@
 #define ADDR_PTR uint64_t
 #define CYCLES uint32_t
 
-#define CHANNEL_DEFAULT_INTERVAL            0x00008000
-#define CHANNEL_SYNC_TIMEMASK_DEF           0x000FFFFF
-#define CHANNEL_SYNC_JITTER_DEF             0x0100
+#define CHANNEL_DEFAULT_INTERVAL 0x00008000
+#define CHANNEL_SYNC_TIMEMASK_DEF 0x000FFFFF
+#define CHANNEL_SYNC_JITTER_DEF 0x0100
 
-#define DEFAULT_FILE_NAME ((char*) SHARED_READONLY_FILE_PATH)
-#define DEFAULT_FILE_OFFSET	0x0
-#define DEFAULT_FILE_SIZE	((uint64_t)(SHARED_ARRAY_SZ + 1024*1024))
-#define CACHE_BLOCK_SIZE	64
-
+#define DEFAULT_FILE_NAME ((char *)SHARED_READONLY_FILE_PATH)
+#define DEFAULT_FILE_OFFSET 0x0
+#define DEFAULT_FILE_SIZE ((uint64_t)(SHARED_ARRAY_SZ + 1024 * 1024))
+#define CACHE_BLOCK_SIZE 64
 
 struct config {
   ADDR_PTR addr;
@@ -49,65 +48,57 @@ struct config {
 
 // ------ Function Definitions  ----------
 
-CYCLES measure_one_block_access_time(ADDR_PTR addr)
-{
-    CYCLES cycles;
+CYCLES measure_one_block_access_time(ADDR_PTR addr) {
+  CYCLES cycles;
 
-    asm volatile("mov %1, %%r8\n\t"
-            "lfence\n\t"
-            "rdtsc\n\t"
-            "mov %%eax, %%edi\n\t"
-            "mov (%%r8), %%r8\n\t"
-            "lfence\n\t"
-            "rdtsc\n\t"
-            "sub %%edi, %%eax\n\t"
-    : "=a"(cycles) /*output*/
-    : "r"(addr)
-    : "r8", "edi");
+  asm volatile("mov %1, %%r8\n\t"
+               "lfence\n\t"
+               "rdtsc\n\t"
+               "mov %%eax, %%edi\n\t"
+               "mov (%%r8), %%r8\n\t"
+               "lfence\n\t"
+               "rdtsc\n\t"
+               "sub %%edi, %%eax\n\t"
+               : "=a"(cycles) /*output*/
+               : "r"(addr)
+               : "r8", "edi");
 
-    return cycles;
+  return cycles;
 }
 
-/* 
- * Returns Time Stamp Counter 
+/*
+ * Returns Time Stamp Counter
  */
-inline __attribute__((always_inline))
-CYCLES rdtscp(void) {
-	CYCLES cycles;
-	asm volatile ("rdtscp"
-	: /* outputs */ "=a" (cycles));
+inline __attribute__((always_inline)) CYCLES rdtscp(void) {
+  CYCLES cycles;
+  asm volatile("rdtscp" : /* outputs */ "=a"(cycles));
 
-	return cycles;
+  return cycles;
 }
 
-/* 
- * Gets the value Time Stamp Counter 
+/*
+ * Gets the value Time Stamp Counter
  */
-inline CYCLES get_time() {
-    return rdtscp();
-}
+inline CYCLES get_time() { return rdtscp(); }
 
 /* Synchronizes at the overflow of a counter
  *
  * Counter is created by masking the lower bits of the Time Stamp Counter
  * Sync done by spinning until the counter is less than CHANNEL_SYNC_JITTER
  */
-inline __attribute__((always_inline))
-CYCLES cc_sync(int CHANNEL_SYNC_TIMEMASK, int CHANNEL_SYNC_JITTER) {
-  while((get_time() & CHANNEL_SYNC_TIMEMASK) > CHANNEL_SYNC_JITTER) {}
+inline __attribute__((always_inline)) CYCLES cc_sync(int CHANNEL_SYNC_TIMEMASK,
+                                                     int CHANNEL_SYNC_JITTER) {
+  while ((get_time() & CHANNEL_SYNC_TIMEMASK) > CHANNEL_SYNC_JITTER) {
+  }
   return get_time();
 }
-
 
 /*
  * Flushes the cache block accessed by a virtual address out of the cache
  */
-inline __attribute__((always_inline))
-void clflush(ADDR_PTR addr)
-{
-    asm volatile ("clflush (%0)"::"r"(addr));
+inline __attribute__((always_inline)) void clflush(ADDR_PTR addr) {
+  asm volatile("clflush (%0)" ::"r"(addr));
 }
-
 
 /*
  * Prints help menu
@@ -119,74 +110,75 @@ void print_help() {
 }
 
 /*
- * Parses the arguments and flags of the program and initializes the struct config
- * with those parameters (or the default ones if no custom flags are given).
+ * Parses the arguments and flags of the program and initializes the struct
+ * config with those parameters (or the default ones if no custom flags are
+ * given).
  */
-void init_config(struct config *config, uint64_t& NUM_BITS,  int argc, char **argv)
-{
-	// Initialize default config parameters
-	int offset = DEFAULT_FILE_OFFSET;
-	config->sync_interval = CHANNEL_DEFAULT_INTERVAL;
-    config->comm_interval = CHANNEL_DEFAULT_INTERVAL;
-    config->CHANNEL_SYNC_TIMEMASK = CHANNEL_SYNC_TIMEMASK_DEF;
-    config->CHANNEL_SYNC_JITTER = CHANNEL_SYNC_JITTER_DEF;
-    
-    char *filename = DEFAULT_FILE_NAME;
+void init_config(struct config *config, uint64_t &NUM_BITS, int argc,
+                 char **argv) {
+  // Initialize default config parameters
+  int offset = DEFAULT_FILE_OFFSET;
+  config->sync_interval = CHANNEL_DEFAULT_INTERVAL;
+  config->comm_interval = CHANNEL_DEFAULT_INTERVAL;
+  config->CHANNEL_SYNC_TIMEMASK = CHANNEL_SYNC_TIMEMASK_DEF;
+  config->CHANNEL_SYNC_JITTER = CHANNEL_SYNC_JITTER_DEF;
 
-    
-	// Parse the command line flags
-	//      -f is used to specify the shared file 
-	//      -i is used to specify the sending interval rate
-	//      -o is used to specify the shared file offset
-    //      -n is used to specify number of bits to transmit.
-	int option;
-	while ((option = getopt(argc, argv, "i:s:o:f:n:")) != -1) {
-      switch (option) {
-      case 'i':
-        config->sync_interval = atoi(optarg);
-        break;
-      case 's':
-        config->CHANNEL_SYNC_TIMEMASK = atoi(optarg);
-        break;        
-      case 'o':
-        offset = atoi(optarg)*CACHE_BLOCK_SIZE;
-        break;
-      case 'f':
-        filename = optarg;
-        break;
-      case 'n':
-        NUM_BITS = strtol(optarg,NULL,10);
-        break;        
-      case 'h':
-        print_help();
-        exit(1);
-      case '?':
-        fprintf(stderr, "Unknown option character\n");
-        print_help();
-        exit(1);
-      default:
-        print_help();
-        exit(1);
-      }
-	}
+  char *filename = DEFAULT_FILE_NAME;
 
-	// Map file to virtual memory and extract the address at the file offset
-	if (filename != NULL) {
-		int inFile = open(filename, O_RDONLY);
-		if(inFile == -1) {
-			printf("Failed to Open File\n");
-			exit(1);
-		}
+  // Parse the command line flags
+  //      -f is used to specify the shared file
+  //      -i is used to specify the sending interval rate
+  //      -o is used to specify the shared file offset
+  //      -n is used to specify number of bits to transmit.
+  int option;
+  while ((option = getopt(argc, argv, "i:s:o:f:n:")) != -1) {
+    switch (option) {
+    case 'i':
+      config->sync_interval = atoi(optarg);
+      break;
+    case 's':
+      config->CHANNEL_SYNC_TIMEMASK = atoi(optarg);
+      break;
+    case 'o':
+      offset = atoi(optarg) * CACHE_BLOCK_SIZE;
+      break;
+    case 'f':
+      filename = optarg;
+      break;
+    case 'n':
+      NUM_BITS = strtol(optarg, NULL, 10);
+      break;
+    case 'h':
+      print_help();
+      exit(1);
+    case '?':
+      fprintf(stderr, "Unknown option character\n");
+      print_help();
+      exit(1);
+    default:
+      print_help();
+      exit(1);
+    }
+  }
 
-		void *mapaddr = mmap(NULL,DEFAULT_FILE_SIZE,PROT_READ,MAP_SHARED,inFile,0);
+  // Map file to virtual memory and extract the address at the file offset
+  if (filename != NULL) {
+    int inFile = open(filename, O_RDWR);
+    if (inFile == -1) {
+      printf("Failed to Open File\n");
+      exit(1);
+    }
 
-		if (mapaddr == (void*) -1 ) {
-			printf("Failed to Map Address\n");
-			exit(1);
-		}
+    void *mapaddr =
+        mmap(NULL, DEFAULT_FILE_SIZE, PROT_WRITE, MAP_SHARED, inFile, 0);
 
-		config->addr = (ADDR_PTR) mapaddr + offset;
-	}
+    if (mapaddr == (void *)-1) {
+      printf("Failed to Map Address\n");
+      exit(1);
+    }
+
+    config->addr = (ADDR_PTR)mapaddr + offset;
+  }
 }
 
 #endif
--- src/utils.hh
+++ src/utils.hh
@@ -1,10 +1,10 @@
 // Copyright (C) 2020, Gururaj Saileshwar
 
-// Commentary: 
+// Commentary:
 // Generic Header file with defines.
-// 
-// 
-// 
+//
+//
+//
 
 #ifndef UTILS_H_
 #define UTILS_H_
@@ -12,117 +12,133 @@
 #define _GNU_SOURCE 1
 #include <sched.h>
 
-#include <string>
-#include <stdio.h>
 #include <cstdint>
 #include <pthread.h>
+#include <stdio.h>
+#include <string>
 #include <unistd.h>
 
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <unistd.h> 
-#include <sys/types.h>
+#include <errno.h>
 #include <sys/syscall.h>
-#include <errno.h> 
+#include <sys/time.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <time.h>
-#include <sys/time.h>
 #include <tr1/random>
+#include <unistd.h>
 
 #include <vector>
 
-/* 
+/*
  * Header Files
  */
 #ifdef _MSC_VER
 #include <intrin.h> /* for rdtscp and clflush */
-#pragma optimize("gt",on)
+#pragma optimize("gt", on)
 #else
 #include <x86intrin.h> /* for rdtscp and clflush */
 #endif
 
-#include "mastik.hh" /* for a helper function: delayloop(cycles)  */
-#include "bits_util.hh" /* for converting string to bits. */
-#include "fec_secded7264.hh"  /* for ECC (Hamming Codes 72,64) */
+#include "bits_util.hh"      /* for converting string to bits. */
+#include "fec_secded7264.hh" /* for ECC (Hamming Codes 72,64) */
+#include "mastik.hh"         /* for a helper function: delayloop(cycles)  */
 
 #define VERBOSE (0)
 
-/* 
+/*
  * Other Defines
  */
 
-//SYSTEM-SPECIFIC DEFINES
-#define CACHE_SZ (8*1024*1024)
+// SYSTEM-SPECIFIC DEFINES
+#define CACHE_SZ (8 * 1024 * 1024)
 #define SYS_FREQ_MHZ (3900)
-#define LLC_MISS_THRESHOLD_CYCLES (180)
-#define SHARED_READONLY_FILE_PATH ("/home/gururaj/Documents/Research/streamline/PUBLIC_ASPLOS21_REPO/streamline_two/shared_readonly_file.txt")
+#define LLC_MISS_THRESHOLD_CYCLES (167)
+#define SHARED_READONLY_FILE_PATH                                              \
+  ("/home/robin/repo/CS7292/streamline/shared_readonly_file.txt")
 
 // Other Sizes
-#define PAGE_SZ (4*1024)
+#define PAGE_SZ (4 * 1024)
 #define CACHELINE_SZ (64)
-#define CL_IN_PAGE (PAGE_SZ/CACHELINE_SZ)
+#define CL_IN_PAGE (PAGE_SZ / CACHELINE_SZ)
 #define ARRENTRY_SZ (8)
 #ifndef ARRAYSZ_PER_CACHESZ
-#define SHARED_ARRAY_SZ ((uint64_t)((2*CACHE_SZ*4)))
+#define SHARED_ARRAY_SZ ((uint64_t)((2 * CACHE_SZ * 4)))
 #else
-#define SHARED_ARRAY_SZ ((uint64_t)((ARRAYSZ_PER_CACHESZ*CACHE_SZ)))
+#define SHARED_ARRAY_SZ ((uint64_t)((ARRAYSZ_PER_CACHESZ * CACHE_SZ)))
 #endif
-#define SHARED_ARRAY_NUMENTRIES ((uint64_t)(SHARED_ARRAY_SZ/ARRENTRY_SZ))
-#define ENTRY_PER_PAGE (PAGE_SZ/ARRENTRY_SZ)
-#define ENTRY_PER_CL (CACHELINE_SZ/ARRENTRY_SZ)
+#define SHARED_ARRAY_NUMENTRIES ((uint64_t)(SHARED_ARRAY_SZ / ARRENTRY_SZ))
+#define ENTRY_PER_PAGE (PAGE_SZ / ARRENTRY_SZ)
+#define ENTRY_PER_CL (CACHELINE_SZ / ARRENTRY_SZ)
 // Offsets for Addresses in Shared File (used for communication)
 #define OFFSET_FR_SYNC_INIT (0)
-#define OFFSET_FR_SYNC_REG_RX1 (1*4096)
-#define OFFSET_FR_SYNC_REG_TX1 (2*4096)
-#define OFFSET_FR_SYNC_REG_RX2 (3*4096)
-#define OFFSET_FR_SYNC_REG_TX2 (4*4096)
-#define OFFSET_FR_SYNC_REG_RX3 (5*4096)
-#define OFFSET_FR_SYNC_REG_TX3 (6*4096)
-#define OFFSET_SHARED_ARRAY    (7*4096)
+#define OFFSET_FR_SYNC_REG_RX1 (1 * 4096)
+#define OFFSET_FR_SYNC_REG_TX1 (2 * 4096)
+#define OFFSET_FR_SYNC_REG_RX2 (3 * 4096)
+#define OFFSET_FR_SYNC_REG_TX2 (4 * 4096)
+#define OFFSET_FR_SYNC_REG_RX3 (5 * 4096)
+#define OFFSET_FR_SYNC_REG_TX3 (6 * 4096)
+#define OFFSET_SHARED_ARRAY (7 * 4096)
 // For restricting size of debugging data-structures
 #define NUM_BITS_DEBUG_MAX (1000000)
 
-
-/* 
+/*
  * Function to Print Scheduling Policy Adopted for current Thread.
  */
-static int display_thread_sched_attr(void)
-{
+static int display_thread_sched_attr(void) {
   int policy, s;
   struct sched_param param;
 
   s = pthread_getschedparam(pthread_self(), &policy, &param);
-  if (s != 0) { printf("pthread_getschedparam"); return 1; }
+  if (s != 0) {
+    printf("pthread_getschedparam");
+    return 1;
+  }
 
   printf("policy=%s, priority=%d\n",
-         (policy == SCHED_FIFO)  ? "SCHED_FIFO" : (policy == SCHED_RR)    ? "SCHED_RR" : (policy == SCHED_OTHER) ? "SCHED_OTHER" : "???",
+         (policy == SCHED_FIFO)
+             ? "SCHED_FIFO"
+             : (policy == SCHED_RR)
+                   ? "SCHED_RR"
+                   : (policy == SCHED_OTHER) ? "SCHED_OTHER" : "???",
          param.sched_priority);
 
   return 0;
 }
 
-/* 
+/*
  * Function to Check if Scheduling Policy Correctly Applied
  */
-static int fail_if_pthrattr_mismatch(int check_policy, int check_prio, int cpu)
-{
+static int fail_if_pthrattr_mismatch(int check_policy, int check_prio,
+                                     int cpu) {
   int policy, s;
   struct sched_param param;
 
   s = pthread_getschedparam(pthread_self(), &policy, &param);
-  if (s != 0) { printf("Error:pthread_getschedparam\n"); exit(1); }
-  if (policy != check_policy) {printf("Error:sched_policy mismatch\n"); exit(1); }
-  if (param.sched_priority != check_prio) {printf("Error:sched_priority mismatch\n"); exit(1); }
-  if (sched_getcpu() != cpu) {printf("Error:sched_priority mismatch\n"); exit(1); }
+  if (s != 0) {
+    printf("Error:pthread_getschedparam\n");
+    exit(1);
+  }
+  if (policy != check_policy) {
+    printf("Error:sched_policy mismatch\n");
+    exit(1);
+  }
+  if (param.sched_priority != check_prio) {
+    printf("Error:sched_priority mismatch\n");
+    exit(1);
+  }
+  if (sched_getcpu() != cpu) {
+    printf("Error:sched_priority mismatch\n");
+    exit(1);
+  }
 
   return 0;
 }
 
-
-
 #endif
 
-// 
+//
 // utils.hh ends here
